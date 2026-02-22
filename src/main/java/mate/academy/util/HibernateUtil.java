7feab9f1087837f576eb148755ee5e2db3e13d26
Szymon Twardowski <szytwardowski@gmail.com>
2026-02-22 21:21:30 +0100

    hw

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -4,14 +4,17 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-
     private static final SessionFactory factory = initSessionFactory();
+
+    // To jest ten prywatny konstruktor, którego wymaga test:
+    private HibernateUtil() {
+    }
 
     private static SessionFactory initSessionFactory() {
         try {
             return new Configuration().configure().buildSessionFactory();
         } catch (Exception e) {
-            throw new RuntimeException("Nie udało sie stworzyć fabryki sesji!", e);
+            throw new RuntimeException("Nie udało się stworzyć fabryki sesji!", e);
         }
     }
 
